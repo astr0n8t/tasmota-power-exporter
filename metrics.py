5f95a8db48d56b4b72783f01c39ff726fce70112
Nathan Higley <contact@nathanhigley.com>
2022-01-14 13:56:41 -0500

    Able to scrape

---
+++ metrics.py
@@ -0,0 +1,36 @@
+import requests
+import os
+from dotenv import load_dotenv
+from flask import Flask
+app = Flask(__name__)
+
+def get_values(ip, user=None, password=None):
+
+    url = 'http://' + ip + '/?m=1'
+
+    session = requests.Session()
+    
+    if user and password:
+        session.auth = (user, password)
+
+    page = session.get(url)
+
+    values = {}
+
+    string_values = str(page.text).split("{s}")
+    for i in range(1,len(string_values)-1):
+        label = string_values[i].split("{m}")[0]
+        value = string_values[i].split("{m}")[1].split("{e}")[0]
+        values[label] = value
+
+    return values
+
+@app.route('/metrics')
+def return_metrics():
+    load_dotenv()
+    ip = os.getenv('DEVICE_IP')
+    user = os.getenv('USER')
+    password = os.getenv('PASSWORD')
+
+
+    return(get_values(ip, user, password))
