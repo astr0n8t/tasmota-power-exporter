8f19cef2dace46eb5ae1e122ed7743460996dd6c
Nathan Higley <contact@nathanhigley.com>
2022-01-14 15:16:13 -0500

    Finished code

--- metrics.py
+++ metrics.py
@@ -1,44 +1,61 @@
 import requests
-import os
-from dotenv import load_dotenv
-from flask import Flask
-app = Flask(__name__)
+from os import getenv
+from time import sleep
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
+from prometheus_client import start_http_server
 
-load_dotenv()
-ip = os.getenv('DEVICE_IP')
-user = os.getenv('USER')
-password = os.getenv('PASSWORD')
+class TasmotaCollector(object):
+    def __init__(self):
+        self.ip = getenv('DEVICE_IP')
+        if not self.ip:
+            self.ip = "192.168.4.1"
+        self.user = getenv('USER')
+        self.password = getenv('PASSWORD')
 
-def get_values():
+    def collect(self):
 
-    url = 'http://' + ip + '/?m=1'
+        response = self.fetch()
 
-    session = requests.Session()
+        for key in response:
+            metric_name = "tasmota_" + key.lower().replace(" ", "_") 
+            metric = response[key].split()[0]
+            unit = None
+            if len(response[key].split()) > 1:
+                unit = response[key].split()[1]
+            g = GaugeMetricFamily(metric_name, key, labels=['device'], unit=unit)
+            g.add_metric([self.ip], metric)
+            yield g
+
+    def fetch(self):
+
+        url = 'http://' + self.ip + '/?m=1'
+
+        session = requests.Session()
+        
+        if self.user and self.password:
+            session.auth = (self.user, self.password)
+
+        page = session.get(url)
+
+        values = {}
+
+        string_values = str(page.text).split("{s}")
+        for i in range(1,len(string_values)-1):
+            label = string_values[i].split("{m}")[0]
+            value = string_values[i].split("{m}")[1].split("{e}")[0]
+            values[label] = value
+
+        return values
+
+if __name__ == '__main__':
+
+    port = getenv('EXPORTER_PORT')
+    if not port:
+        port = 8000
+
+    start_http_server(int(port))
+    REGISTRY.register(TasmotaCollector())
+
+    while(True):
+        sleep(1)
     
-    if user and password:
-        session.auth = (user, password)
-
-    page = session.get(url)
-
-    values = {}
-
-    string_values = str(page.text).split("{s}")
-    for i in range(1,len(string_values)-1):
-        label = string_values[i].split("{m}")[0]
-        value = string_values[i].split("{m}")[1].split("{e}")[0]
-        values[label] = value
-
-    return values
-
-@app.route('/metrics')
-def return_metrics():
-
-    response = ""
-
-    data = get_values()
-
-    for key in data:
-        line = key.lower().replace(" ", "_") + " " + data[key].split()[0] + '\n'
-        response += line
-
-    return(response)
